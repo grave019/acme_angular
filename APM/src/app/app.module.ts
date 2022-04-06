55e696c2aa974059e1045c11c644cd5b3fe3691b
Brent Graves <graves.brent@gmail.com>
2022-04-06 19:15:27 -0400

    added custom pipe to convert a dash to a space displayed on the page.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './Products/product-list.component';
+import { ConvertoSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertoSpacesPipe
   ],
   imports: [
     BrowserModule,
