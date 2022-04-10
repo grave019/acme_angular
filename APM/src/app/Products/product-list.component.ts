de7feeef30d18b1ab59d9dd34662b88ab6a43419
Brent Graves <graves.brent@gmail.com>
2022-04-10 16:39:31 -0400

    added routes for welcome and products pages.

--- APM/src/app/Products/product-list.component.ts
+++ APM/src/app/Products/product-list.component.ts
@@ -3,7 +3,6 @@ import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
