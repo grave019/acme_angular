dfd9d3b42d192b4a7dd0e46795505dcdbbee70a1
Brent Graves <graves.brent@gmail.com>
2022-04-10 17:32:54 -0400

    routes are working correctly with corresponding paths.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -8,9 +8,9 @@ import { ProductListComponent } from './Products/product-list.component';
 import { ConvertoSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './Products/product-detail.component';
-import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductService } from './Products/product.service';
+//import { ProductService } from './Products/product.service';
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
@@ -27,7 +27,7 @@ import { ProductService } from './Products/product.service';
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-    { path: 'products', component: ProductService },
+    { path: 'products', component: ProductListComponent },
     { path: 'products/:id', component: ProductDetailComponent },
     { path: 'welcome', component: WelcomeComponent },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
