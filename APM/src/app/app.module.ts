326ecb2c093ab711faf1aa14633887129ab037f0
Brent Graves <graves.brent@gmail.com>
2022-04-11 15:33:02 -0400

    added correct product components to product.module.ts. App is working as expected.

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,38 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './Products/product-list.component';
-import { ConvertoSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './Products/product-detail.component';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailGuard } from './Products/product-detail.guard';
 import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    ConvertoSpacesPipe,
-    StarComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-    { path: 'products', component: ProductListComponent },
-    { 
-      path: 'products/:id',
-      canActivate: [ProductDetailGuard], 
-      component: ProductDetailComponent },
+   
     { path: 'welcome', component: WelcomeComponent },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
